fa675cedcf893e5c9846c1322f5757f444e083d1
Rostislav Jadavan <rostislav.jadavan@gmail.com>
2018-07-18 20:26:14 +0200

    create layer behaviour

--- src/gui/view/properties_partials.cpp
+++ src/gui/view/properties_partials.cpp
@@ -5,22 +5,27 @@
 #include "../layer/text.hpp"
 
 void atlas::gui::view::PropertiesPartials::createLayerGui(int index) {
-    if (ImGui::TreeNode("Layer type")) {
-        std::shared_ptr<atlas::layer::Base> layer = atlas::core::App::instance().getLayerContainer()->getLayer(index);
-        if (layer->getLayerType() == atlas::layer::LAYER_TYPE_EMPTY) {
-            if (ImGui::Button("GIF DIRECTORY")) {
-                atlas::core::App::instance().getLayerContainer()->setLayer(index, std::make_shared<atlas::layer::GifDirectory>());
-            }
-            ImGui::Text("Play gifs from selected directory.");
-            if (ImGui::Button("TEXT")) {
-                atlas::core::App::instance().getLayerContainer()->setLayer(index, std::make_shared<atlas::layer::Text>());
-            }
-            ImGui::Text("Display user defined text using custom truetype font (.ttf only)");
-        } else {
-            if (ImGui::Button("DELETE")) {
+    std::shared_ptr<atlas::layer::Base> layer = atlas::core::App::instance().getLayerContainer()->getLayer(index);
+    
+    if (layer->getLayerType() == atlas::layer::LAYER_TYPE_EMPTY) {
+        ImGui::Text("This layer is empty. To create layer select one of following type.");
+        
+        if (ImGui::Button("GIF DIRECTORY")) {
+            atlas::core::App::instance().getLayerContainer()->setLayer(index, std::make_shared<atlas::layer::GifDirectory>());
+        }
+        ImGui::Text("Play gifs from selected directory.");
+        if (ImGui::Button("TEXT")) {
+            atlas::core::App::instance().getLayerContainer()->setLayer(index, std::make_shared<atlas::layer::Text>());
+        }
+        ImGui::Text("Display user defined text using custom truetype font (.ttf only)");
+    } else {
+        if (ImGui::TreeNode("Delete layer")) {
+            ImGui::Text("Do you really want to delete this layer?");
+            if (ImGui::Button("YES, DELETE")) {
                 atlas::core::App::instance().getLayerContainer()->setLayer(index, std::make_shared<atlas::layer::Empty>());
             }
+            ImGui::TreePop();
         }
-        ImGui::TreePop();
     }
+    
 }
